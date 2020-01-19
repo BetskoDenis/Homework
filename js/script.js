872e4db5fb38b71ebaa8c70bdac10bdcb098e09a
BetskoDenis <47107535+BetskoDenis@users.noreply.github.com>
2020-01-19 19:24:27 +0200

    008

--- js/script.js
+++ js/script.js
@@ -1,32 +1,75 @@
-let left = document.getElementById('carousel_left_button');
-let right = document.getElementById('carousel_right_button');
-let carouselWrapper = document.getElementById('carousel__wrapper');
-let carouselBlock = document.getElementById('carousel_block');
-let carouselItem = document.getElementsByClassName('carousel_item');
-let carouselItemWidth = carouselWrapper.offsetWidth / carouselItem.length;
-let carouselItemWidthInvisible = carouselWrapper.offsetWidth - carouselBlock.offsetWidth;
-let carouselWrapperMove = 0;
+Main = {
+    slider : function () {
+        let left = document.getElementById('carousel_left_button');
+        let right = document.getElementById('carousel_right_button');
+        let carouselWrapper = document.getElementById('carousel__wrapper');
+        let carouselBlock = document.getElementById('carousel_block');
+        let carouselItem = document.getElementsByClassName('carousel_item_top');
+        let percentItem;
+        let leftPercent;
 
 
-left.onclick = function () {
-    console.log(carouselWrapperMove)
-    if (carouselWrapperMove < -carouselItemWidthInvisible) {
-        carouselWrapperMove = 0;
-        carouselWrapper.style.left = carouselWrapperMove + "px";
-    } else {
-        carouselWrapperMove = carouselWrapperMove - carouselItemWidth;
-        carouselWrapper.style.left = carouselWrapperMove + "px";
-    }
-};
+        function percent() {
+            if (carouselBlock.offsetWidth > 920) {
+                return percentItem = 20;
+            } else if (carouselBlock.offsetWidth <= 920 && carouselBlock.offsetWidth > 738) {
+                return percentItem = 25;
+            } else if (carouselBlock.offsetWidth <= 738 && carouselBlock.offsetWidth > 546) {
+                return percentItem = 33.33;
+            } else if (carouselBlock.offsetWidth <= 546 && carouselBlock.offsetWidth > 360) {
+                return percentItem = 50;
+            } else if (carouselBlock.offsetWidth < 360) {
+                return percentItem = 100;
+            }
+        }
 
-right.onclick = function () {
-    console.log(carouselWrapper.offsetWidth)
-    if(carouselWrapperMove != 0){
-        carouselWrapperMove = carouselWrapperMove + carouselItemWidth;
-         carouselWrapper.style.left = carouselWrapperMove + "px";
-        console.log(carouselWrapperMove);
-    } else {
-        carouselWrapperMove = -carouselItemWidthInvisible-1;
-        carouselWrapper.style.left = carouselWrapperMove + "px";
-    }
+        function leftFunction() {
+            leftPercent = 0;
+            return function () {
+                leftPercent = leftPercent - percentItem;
+                carouselWrapper.style.left = leftPercent + "%";
+            }
+        }
+
+        let leftMove = leftFunction();
+
+        function rightFunction() {
+            return function () {
+                leftPercent = leftPercent + percentItem;
+                carouselWrapper.style.left = leftPercent + "%";
+            }
+        }
+
+        let rightMove = rightFunction();
+
+        left.onclick = function () {
+            percent();
+            let leftPercentPositive = leftPercent * -1;
+            let percentAllItem = percentItem * carouselItem.length;
+            if (leftPercentPositive >= (percentAllItem - 100)) {
+                leftPercent = 0;
+                carouselWrapper.style.left = leftPercent + "%";
+            } else {
+                leftMove();
+            }
+        }
+
+        right.onclick = function () {
+            percent();
+            let percentAllItem = percentItem * carouselItem.length;
+            if (Math.round(leftPercent) == 0) {
+                leftPercent = (percentAllItem - 100) * -1;
+                carouselWrapper.style.left = leftPercent + "%";
+            } else {
+                rightMove();
+            }
+        }
+            },
+    init: function () {
+        this.slider();
+    },
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    Main.init();
+})
