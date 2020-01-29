352651dcce4b275690554ca6790096812321148e
BetskoDenis <47107535+BetskoDenis@users.noreply.github.com>
2020-01-29 21:39:52 +0200

    014

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 main = {
-    slider: function() {
-        let left = document.getElementById('carousel_left_button');
-        let right = document.getElementById('carousel_right_button');
+    slider: function () {
+        let leftButton = document.getElementById('carousel_left_button');
+        let rightButton = document.getElementById('carousel_right_button');
         let carouselWrapper = document.getElementById('carousel__wrapper');
         let carouselBlock = document.getElementById('carousel_block');
         let carouselItem = document.getElementsByClassName('carousel_item_top');
-        let item = carouselBlock.querySelector('.carousel_item_top');
-        let itemWidth = item.clientWidth;
+        let itemWidth = carouselItem[0].clientWidth;
         let carouselBlockWidth = carouselBlock.clientWidth;
         let leftPercent = 0;
         let coordinatesDown = 0;
@@ -20,55 +19,19 @@ main = {
         let down = 'mousedown';
         let up = 'mouseup';
         let move = 'mousemove';
-        let percentItem = 100 / Math.round(carouselBlockWidth/itemWidth);
+        let percentItem = 100 / Math.round(carouselBlockWidth / itemWidth);
         let dropdownMenu = document.getElementById('dropdown_menu_header');
         let dropdownMenuItems = dropdownMenu.getElementsByTagName('a');
         let openChecked = document.getElementById('open');
 
-        function checked(){
-            if(openChecked.checked == false){
-                openChecked.checked = true
-            } openChecked.checked = false
-        };
-
-        for (let dropdownMenuItem of dropdownMenuItems ){
-            dropdownMenuItem.addEventListener('click', function () {
-                let firstVisibleItem = (Math.abs(leftPercent)/Math.abs(percentItem)) + 1;
-                let dropdownMenuItemsArr = Array.from(dropdownMenuItems);
-                let indexActiveItemMenu = (dropdownMenuItemsArr.indexOf(this, 0))+1;
-                let indexMove = ((indexActiveItemMenu - firstVisibleItem)*Math.abs(percentItem)) * -1;
-                leftPercent = leftPercent + indexMove;
-                checked();
-                setTimeout(function () {
-                    carouselWrapper.style.left = leftPercent + '%';
-                },500)
-
-                let carouselItemArr = Array.from(carouselItem);
-                let activeItemCarousel = carouselItemArr[indexActiveItemMenu-1]
-
-                for (let i=0; i<carouselItemArr.length; i++){
-                    carouselItemArr[i].classList.remove('active')
-                }
-                activeItemCarousel.classList.add('active');
-
-            })
-        }
-
-
-
-        // dropdownMenu.addEventListener('click', function () {
-        //
-        // })
-        checked();
-
-        window.addEventListener('resize', function () {
-            itemWidth = item.clientWidth;
+        window.addEventListener('resize', () => {
+            itemWidth = carouselItem[0].clientWidth;
             carouselBlockWidth = carouselBlock.clientWidth;
-            percentItem = 100 / Math.round(carouselBlockWidth/itemWidth);
+            percentItem = 100 / Math.round(carouselBlockWidth / itemWidth);
             leftPercent = 0;
             carouselWrapper.style.left = leftPercent + '%';
 
-        })
+        });
 
         if (touchDevice) {
             down = 'touchstart';
@@ -76,29 +39,67 @@ main = {
             move = 'touchmove';
         }
 
-        function leftFunction() {
-            return function () {
+
+        function checked() {
+            if (openChecked.checked == false) {
+                openChecked.checked = true
+            }
+            openChecked.checked = false
+        }
+
+        for (let dropdownMenuItem of dropdownMenuItems) {
+            dropdownMenuItem.addEventListener('click', function (){
+                let percentAllItem = percentItem * carouselItem.length;
+                let firstVisibleItem = (Math.abs(leftPercent) / Math.abs(percentItem)) + 1;
+                let dropdownMenuItemsArr = Array.from(dropdownMenuItems);
+                let indexActiveItemMenu = (dropdownMenuItemsArr.indexOf(this, 0)) + 1;
+                let indexMove = ((indexActiveItemMenu - firstVisibleItem) * Math.abs(percentItem)) * -1;
+                leftPercent = leftPercent + indexMove;
+
+
+                checked();
+                setTimeout(() => {
+                    if (Math.round(percentAllItem - 100) < (Math.round(leftPercent)) * -1) {
+                        leftPercent = (percentAllItem - 100) * -1;
+                        carouselWrapper.style.left = leftPercent + '%';
+                    } else {
+                        carouselWrapper.style.left = leftPercent + '%';
+                    }
+                }, 500)
+
+                let carouselItemArr = Array.from(carouselItem);
+                let activeItemCarousel = carouselItemArr[indexActiveItemMenu - 1]
+
+                for (let i = 0; i < carouselItemArr.length; i++) {
+                    carouselItemArr[i].classList.remove('active')
+                }
+                activeItemCarousel.classList.add('active');
+
+            });
+        }
+
+        function actionLeftButton() {
+            return () => {
                 if (leftPercent > 0) {
                     leftPercent = leftPercent * -1
                 }
-
                 leftPercent = leftPercent - percentItem;
                 carouselWrapper.style.left = leftPercent + "%";
             }
         }
 
-        let leftMove = leftFunction();
+        let leftMove = actionLeftButton();
 
-        function rightFunction() {
-            return function () {
+        function actionRightButton() {
+            return () => {
                 leftPercent = leftPercent + percentItem;
                 carouselWrapper.style.left = leftPercent + "%";
             }
         }
 
-        let rightMove = rightFunction();
+        let rightMove = actionRightButton();
 
-        left.onclick = function () {
+        rightButton.onclick =  () => {
             let leftPercentPositive = leftPercent;
             if (leftPercent < 0) {
                 leftPercentPositive = leftPercent * -1;
@@ -112,7 +113,7 @@ main = {
             }
         };
 
-        right.onclick = function () {
+        leftButton.onclick = () => {
             if (leftPercent > 0) {
                 leftPercent = leftPercent * -1;
             }
@@ -148,7 +149,7 @@ main = {
                 coordinatesResult = coordinatesDown - coordinatesUp
                 mouseDown = false;
 
-                if(mouseMove){
+                if (mouseMove) {
                     leftPercent = (leftPercent + moveLeft) * -1;
                 }
 
@@ -164,7 +165,7 @@ main = {
 
         });
 
-        carouselBlock.addEventListener(move, function (event) {
+        carouselBlock.addEventListener(move, event => {
             if (mouseDown) {
 
                 if (touchDevice) {
@@ -173,20 +174,29 @@ main = {
                     coordinatesMove = event.x;
                 }
                 mouseMove = true;
-                let resultTemp = coordinatesDown - coordinatesMove;
-                if (resultTemp < 0 && resultTemp >= -200) {
-                    moveLeft = percentItem * -1;
-                } else if (resultTemp < -200 && resultTemp >= -400) {
-                    moveLeft = (2 * percentItem) * -1;
-                } else if (resultTemp < -400) {
-                    moveLeft = (3 * percentItem) * -1;
-                } else if (resultTemp >= 0 && resultTemp <= 200) {
-                    moveLeft = percentItem;
-                } else if (resultTemp > 200 && resultTemp <= 400) {
-                    moveLeft = 2 * percentItem;
-                } else if (resultTemp > 400) {
-                    moveLeft = 3 * percentItem;
+                let resultMove = coordinatesDown - coordinatesMove;
+
+                switch (true) {
+                    case  resultMove < 0 && resultMove >= -200:
+                        moveLeft = percentItem * -1;
+                        break;
+                    case resultMove < -200 && resultMove >= -400:
+                        moveLeft = (2 * percentItem) * -1;
+                        break;
+                    case resultMove < -400:
+                        moveLeft = (3 * percentItem) * -1;
+                        break;
+                    case resultMove >= 0 && resultMove <= 200:
+                        moveLeft = percentItem;
+                        break;
+                    case resultMove > 200 && resultMove <= 400:
+                        moveLeft = 2 * percentItem;
+                        break;
+                    case resultMove > 400:
+                        moveLeft = 3 * percentItem;
+                        break;
                 }
+
                 if (leftPercent < 0) {
                     leftPercent = leftPercent * -1;
                 }
@@ -194,14 +204,13 @@ main = {
             }
         })
     },
-    scroll: function(){
+    scroll: function () {
         let dropdownMenu = document.getElementById('dropdown_menu_header');
         let anchors = dropdownMenu.querySelectorAll('a[href*="#"]');
         for (let anchor of anchors) {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault()
-
-                let blockID = anchor.getAttribute('href').substr(1)
+            anchor.addEventListener('click', e => {
+                e.preventDefault();
+                let blockID = anchor.getAttribute('href').substr(1);
                 document.getElementById(blockID).scrollIntoView({
                     behavior: 'smooth',
                     block: 'start'
@@ -216,6 +225,6 @@ main = {
     },
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     main.init();
 })
